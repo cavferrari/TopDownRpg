86b6e0e262a12600ad83b504149036b497251395
cavferrari <cavferrari@gmail.com>
2023-04-18 20:44:46 -0300

    Fixing skin change mechanism. Adding additional sprites.

--- Assets/Scripts/PlayerCustomize.cs
+++ Assets/Scripts/PlayerCustomize.cs
@@ -17,7 +17,9 @@ public class PlayerCustomize : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private string spriteName;
     private int skinNumber;
-    private string equipedSkin = "Main";
+    private string skinName;
+    private string equipedSkinHeader = "Main";
+    private string spriteHeader;
 
     // Start is called before the first frame update
     void Start()
@@ -28,23 +30,33 @@ public class PlayerCustomize : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        UpdateSkin(equipedSkin);
+        UpdateSkin();
     }
 
-    public void UpdateEquipedSkin(string skinName)
+    public void UpdateEquipedSkin(string skinHeader)
     {
-        equipedSkin = skinName;
+        equipedSkinHeader = skinHeader;
     }
 
-    private void UpdateSkin(string skinName)
+    private void UpdateSkin()
     {
         spriteName = spriteRenderer.sprite.name;
+        spriteHeader = spriteName.Substring(0, spriteName.IndexOf("_") + 1);
         skinNumber = int.Parse(spriteName.Substring(spriteName.Length - 1));
+        skinName = spriteName.Replace(spriteHeader, "");
+        skinName = skinName.Remove(skinName.Length - 2);
+        skinName = equipedSkinHeader + "_" + skinName + "-" + skinNumber;
         for (int i = 0; i < skins.Length; i++)
         {
-            if (skins[i].name.Equals(equipedSkin))
+            if (skins[i].name.Equals(equipedSkinHeader))
             {
-                spriteRenderer.sprite = skins[i].sprites[skinNumber];
+                for (int j = 0; j < skins[i].sprites.Length; j++)
+                {
+                    if (skins[i].sprites[j].name.Equals(skinName))
+                    {
+                        spriteRenderer.sprite = skins[i].sprites[j];
+                    }
+                }
             }
         }
     }
